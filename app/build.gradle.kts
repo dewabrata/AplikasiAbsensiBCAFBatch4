2b1307119c54986a9ac07db1005c7e0841d5f373
dewabrata <dewabrata@gmail.com>
2024-10-14 11:47:12 +0700

    Add Service and HeaderInterceptor

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,6 +44,8 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.logging.interceptor)
+    implementation(libs.okhttp)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
