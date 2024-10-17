3888e7f4cc1b28216e5d65acf823e6c588a1e8ab
dewabrata <dewabrata@gmail.com>
2024-10-17 14:14:23 +0700

    Add GPS Tracking Service

--- app/src/main/java/com/juaracoding/aplikasiabsensi/bgservice/LocationTrackingService.kt
+++ app/src/main/java/com/juaracoding/aplikasiabsensi/bgservice/LocationTrackingService.kt
@@ -32,7 +32,7 @@ class LocationTrackingService : Service() {
             override fun onLocationResult(p0: LocationResult) {
                 super.onLocationResult(p0)
                 for(location in p0.locations)
-                Log.d("Location Provider :", " "+location.altitude + " " + location.longitude)
+                 Log.d("Location Provider :", " "+location.altitude + " " + location.longitude)
             }
         }
 
