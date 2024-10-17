75c4ffbd7a3b25e698e13af557428814a85f928c
dewabrata <dewabrata@gmail.com>
2024-10-17 15:49:11 +0700

    Penambahan WebViewTutorial

---
+++ app/src/main/java/com/juaracoding/aplikasiabsensi/WebViewTutorial.kt
@@ -0,0 +1,40 @@
+package com.juaracoding.aplikasiabsensi
+
+import android.os.Bundle
+import android.webkit.JavascriptInterface
+import android.webkit.WebChromeClient
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class WebViewTutorial : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_web_view_tutorial)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val webView = findViewById<WebView>(R.id.webView)
+        webView.webViewClient = WebViewClient()
+        webView.webChromeClient = WebChromeClient()
+        webView.settings.javaScriptEnabled = true
+        webView.addJavascriptInterface(WebAppInterface(), "AndroidInterface")
+
+        webView.loadUrl("https://3f50-103-165-222-114.ngrok-free.app/webview/")
+    }
+
+    inner class WebAppInterface {
+        @JavascriptInterface
+        fun showToast(toast: String) {
+            Toast.makeText(applicationContext, toast, Toast.LENGTH_SHORT).show()
+        }
+    }
+}
